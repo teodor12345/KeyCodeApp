b4c1276339c7adbd697e864f7633bf4dca6f39ca
Teodor Madzovski <ted.madzovski@gmail.com>
2022-03-24 12:15:31 +0100

    1 commit
    HTML CSS JS - DONE

---
+++ MainJS.js
@@ -0,0 +1,24 @@
+const insert = document.getElementById('insert')
+
+
+window.addEventListener('keydown', (event)=>{
+    insert.innerHTML = `
+    <div class="key">
+    ${event.key === ' ' ? 'Space' : event.key} 
+    <small>event key</small>
+
+</div>
+
+<div class="key">
+    ${event.keyCode}
+
+    <small>event key code</small>
+</div>
+
+<div class="key">
+     ${event.code}
+     <small>event key code</small>
+
+</div>`
+
+})
